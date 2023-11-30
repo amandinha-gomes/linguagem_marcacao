e7905ac4f31e19a2a5c1eeb17eea141dc6cfd302
Amanda <134006882+amandinha-gomes@users.noreply.github.com>
2023-11-30 16:49:33 -0300

    Delete ProjetoPizzaria directory

--- ProjetoPizzaria/js/menu.js
+++
@@ -1,9 +0,0 @@
-const pizzas = require('./arrayPizzas.json')
-
-function listarPreco(pizzas) {
-    return pizzas.filter((pizza) => {
-        return pizza
-    }
-    )
-}
-console.log(listarPreco(pizzas));
